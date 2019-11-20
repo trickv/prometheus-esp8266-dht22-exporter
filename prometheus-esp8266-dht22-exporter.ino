49dede049eba3b0da2c980c5296845ddc56cfdca
Patrick van Staveren <trick@vanstaveren.us>
2019-11-20 08:02:10 -0600

    remove unused 404 handler from example code

--- prometheus-esp8266-dht22-exporter.ino
+++ prometheus-esp8266-dht22-exporter.ino
@@ -39,31 +39,14 @@ void handleRoot() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void handleNotFound() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
 void setup(void) {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
-  dht.begin();
   Serial.println("Initialized. Waiting for WiFi");
 
+  dht.begin();
   char device_id_char[16];
   sprintf(device_id_char, "ESP_%06X", ESP.getChipId());
   device_id = String(device_id_char);
