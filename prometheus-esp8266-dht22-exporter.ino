4448aaabec105e421bb529f48c0968979e0bcfa9
Patrick van Staveren <trick@vanstaveren.us>
2019-12-31 22:10:50 -0600

    add ArduinoOTA updating support

--- prometheus-esp8266-dht22-exporter.ino
+++ prometheus-esp8266-dht22-exporter.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <DHT.h>
+#include <ArduinoOTA.h>
 
 #define DHTPIN 0 // GPIO0, AKA "D3" on the D1 mini
 #define DHTTYPE DHT22
@@ -73,6 +74,41 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  ArduinoOTA.setPasswordHash("f9f9c5fd6f84c7b3d2c4fb05cafcfcae");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
   server.on("/", handleRoot);
 
   server.onNotFound([]() {
@@ -84,5 +120,6 @@ void setup(void) {
 }
 
 void loop(void) {
+  ArduinoOTA.handle();
   server.handleClient();
 }
